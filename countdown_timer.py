aa64f2133ec54928aff15f85d1d6e49bce220983
Vaishu-1611 <vmhetre608@gmail.com>
2026-04-19 17:22:32 +0530

    updated

--- countdown_timer.py
+++ countdown_timer.py
@@ -2,3 +2,9 @@ import time
 t = int(input("Enter the time in seconds: "))
 for x in range(t, 0, -1):
     sec = x % 60
+    min = int(x/60)%60
+    hr = int(x/3600)
+    print(f"{hr:02d}:{min:02d}:{sec:02d}")
+    time.sleep(1)
+
+print("Time's up!")
